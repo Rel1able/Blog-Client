671f82032616c47a90b72fa36bcc8f5c9dda8629
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-04-17 13:36:03 +0200

    displayed error messages during signing in

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -8,17 +8,34 @@ export default function SignUp() {
     let navigate = useNavigate();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [confPassword, setConfPassword] = useState("");
+    const [errors, setErrors] = useState([]);
 
     
     async function handleSubmit(e) {
         e.preventDefault();
-        await fetch("https://blog-api-rrvr.onrender.com/sign-up", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({username, password})
-        })
+        setErrors([]);
+        try {
+            const res = await fetch("https://blog-api-rrvr.onrender.com/sign-up", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({username, password, confPassword})
+            })
+            const data = await res.json();
+            if (!res.ok) {
+                if (data.errors) {
+                    setErrors(data.errors.map(err => err.msg));
+                } else {
+                    setErrors(["An unexpected error occurred"])
+                }
+                return
+            }
+        } catch (err) {
+            setErrors(["Server is unreachable. Try again later"])
+        }
+        
         navigate("/login");
     }
 
@@ -38,11 +55,20 @@ export default function SignUp() {
                     </div>
                     <div className={styles.labelInputContainer}>
                         <label htmlFor="confPassword">Confirm Password</label>
-                        <input type="password" id="confPassword" name="confPassword" value={password} onChange={e => setPassword(e.target.value)} />
+                        <input type="password" id="confPassword" name="confPassword" value={confPassword} onChange={e => setConfPassword(e.target.value)} />
                     </div>
                     <p>Already have an account? <Link className={styles.login} to="/login">Log in</Link></p>
                     <button className={styles.btn} type="submit">Sign Up</button>
                 </form>
+                {
+                        errors.length > 0 && (
+                            <ul className={styles.errorList}>
+                            {errors.map((err, id) => (
+                                    <li className={styles.error} key={id}>{err}</li>
+                                ))}
+                            </ul>
+                        )
+                    }
             </div>
             
         </>
